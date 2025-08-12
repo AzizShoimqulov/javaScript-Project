7c958509a9afd20e26d42cfb983fadc4d4d4f8fb
AzizShoimqulov <azizshoimqulov74@gmail.com>
2025-08-12 12:11:19 +0500

    sdfsa

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-    // fetch('https://jsonplaceholder.typicode.com/posts')
-    //   .then(response => response.json())
-    //   .then(posts => {
-    //     const container = document.getElementById('posts-container');
-    //     container.innerHTML = '';
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(posts => {
+        const container = document.getElementById('posts-container');
+        container.innerHTML = '';
 
-    //     posts.forEach(post => {
-    //       const div = document.createElement('div');
-    //       div.className = 'post';
-    //       div.innerHTML = `
-    //         <h2>${post.title}</h2>
-    //         <p>${post.body}</p>
-    //       `;
-    //       container.appendChild(div);
-    //     });
-    //   })
-    //   .catch(error => {
-    //     document.getElementById('posts-container').innerText = 'Xatolik yuz berdi: ' + error;
-    //   });
+        posts.forEach(post => {
+          const div = document.createElement('div');
+          div.className = 'post';
+          div.innerHTML = `
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+          `;
+          container.appendChild(div);
+        });
+      })
+      .catch(error => {
+        document.getElementById('posts-container').innerText = 'Xatolik yuz berdi: ' + error;
+      });
 
 
 
-    const menuToggle = document.getElementById("menu-toggle");
+
+
+
+
+      const menuToggle = document.getElementById("menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
 menuToggle.addEventListener("click", () => {
