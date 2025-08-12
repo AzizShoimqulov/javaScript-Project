65042fbaf9a26397530630af9066b944fe87c020
AzizShoimqulov <azizshoimqulov74@gmail.com>
2025-08-12 12:17:09 +0500

    asd

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(posts => {
-        const container = document.getElementById('posts-container');
-        container.innerHTML = '';
+fetch('https://api.escuelajs.co/api/v1/products')
+  .then(response => response.json())
+  .then(posts => {
+    const container = document.getElementById('posts-container');
+    container.innerHTML = '';
 
-        posts.forEach(post => {
-          const div = document.createElement('div');
-          div.className = 'post';
-          div.innerHTML = `
-            <h2>${post.title}</h2>
-            <p>${post.body}</p>
-          `;
-          container.appendChild(div);
-        });
-      })
-      .catch(error => {
-        document.getElementById('posts-container').innerText = 'Xatolik yuz berdi: ' + error;
-      });
-
-
+    posts.forEach(product => {
+      const div = document.createElement('div');
+      div.className = 'post';
+      div.innerHTML = `
+        <img src="${product.images[0]}" alt="${product.title}" width="200">
+        <h2>${product.title}</h2>
+        <p><strong>Price:</strong> $${product.price}</p>
+      `;
+      container.appendChild(div);
+    });
+  })
+  .catch(error => {
+    document.getElementById('posts-container').innerText = 'Xatolik yuz berdi: ' + error;
+  });
 
 
 
