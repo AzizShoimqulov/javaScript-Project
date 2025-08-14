31aa0d0bc44feadf2f3cb1ba6ce7061e350e1d35
AzizShoimqulov <azizshoimqulov74@gmail.com>
2025-08-14 10:42:15 +0500

    sdfg

--- script.js
+++ script.js
@@ -111,3 +111,31 @@ fetch('https://jsonplaceholder.typicode.com/photos')
 //   navLinks.classList.toggle("active");
 // });
 
+
+fetch('https://jsonplaceholder.typicode.com/photos')
+//   .then(response => response.json())
+//   .then(photos => {
+//     const container = document.getElementById('posts-container');
+//     container.innerHTML = '';
+
+//     photos.slice(0, 50).forEach(photo => {
+//       const div = document.createElement('div');
+//       div.className = 'post';
+//       div.innerHTML = `
+//         <img src="${photo.thumbnailUrl}" alt="${photo.title}">
+//         <p>${photo.title}</p>
+//       `;
+//       container.appendChild(div);
+//     });
+//   })
+//   .catch(error => {
+//     document.getElementById('posts-container').innerText = 'Xatolik yuz berdi: ' + error;
+//   });
+
+// const menuToggle = document.getElementById("menu-toggle");
+// const navLinks = document.querySelector(".nav-links");
+
+// menuToggle.addEventListener("click", () => {
+//   navLinks.classList.toggle("active");
+// });
+
